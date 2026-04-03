5a268348bed9cd4bc5c3b0d3d0abf04d0ad4bc10
rclador-2026 <rclador@gmail.com>
2026-04-02 22:40:39 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 client = genai.Client(api_key=GEMINI_API_KEY)
-URL_SHEETS = "https://script.google.com/macros/s/AKfycby0dH7_UYdbFBsKqhGt7vTG3zFnc51rKYL8xY0vYkvWkhUFcnWX5RWUwFezTf8IlhK7/exec"
+URL_SHEETS = "https://script.google.com/macros/s/AKfycbyI693LEuexsonUvLWVRolzJY9gEmFQoIy-Q5DIG_Gx1pLlr-kGEdwHu6DDEejp9j3x/exec"
 
 MAX_HISTORIAL = 10  # Máximo de mensajes a recordar por alumno
 
